daadac9acfbbb43052b58301436ca6dec88fb34f
Ulyana <gjkzyjdbx@mail.ru>
2023-03-13 17:06:03 +0300

    Second Commid

--- monitor.cs
+++ monitor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace практика
 {
@@ -11,5 +12,12 @@ namespace практика
         public string firm { get; set; }
         public string type { get; set; }
         public float diagonal { get; set; }
+        //public void Print()
+        //{
+        //   firm = "Samsung";
+        //   type = "жидкокристаллический";
+        //   diagonal = 32;
+        //}
     }
+
 }
