789c408574bcb2b642a30e65a4f22b00b96067c4
Ulyana <gjkzyjdbx@mail.ru>
2023-03-13 16:56:42 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace практика
+{
+    internal class monitor
+    {
+        static void Main(string[] args)
+        {
+            public string firm;
+            public string type;
+            public float diagonal;
+
+        }
+    }
+}
---
+++ monitor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace практика
+{
+    internal class monitor
+    {
+        public string firm { get; set; }
+        public string type { get; set; }
+        public float diagonal { get; set; }
+    }
+}
